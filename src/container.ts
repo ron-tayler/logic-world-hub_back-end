92b02296a944ba29b0554c0cf080b727f2b6ceec
ron-tayler <max.urs@yandex.ru>
2023-02-05 17:27:57 +0300

    Use provider

--- src/container.ts
+++ src/container.ts
@@ -1,15 +1,37 @@
 import {Container} from "inversify";
 import {PrismaClient} from "@/../prisma/generated/lwh";
-import "@/App/Controllers/Mods";
-import {ModelMod} from "@/App/Model/Mods/Mod";
 import {SelectelStorage} from "@/Services/SelectelStorage";
+import { buildProviderModule } from "inversify-binding-decorators";
+import nodemailer from "nodemailer"
+
+// Middlewares
+import "@/App/Middlewares/Check/Auth"
+import "@/App/Middlewares/Check/AuthAccess"
+
+// Контролеры
+import "@/App/Controllers/Mods"
+import "@/App/Controllers/Test"
+
+// Модели
+import "@/App/Models/Mods/Mod";
 
 const sel_user = process.env["S3_USER"] ?? ""
 const sel_pass = process.env["S3_PASSWORD"] ?? ""
 
+const email_transport = nodemailer.createTransport({
+    host: "server142.hosting.reg.ru",
+    secure: true,
+    auth:{
+        user: "logic-world-hub@room-seven.ru",
+        pass: "hY9bN8yM1h"
+    }
+})
 export const container = new Container()
 export const prisma = new PrismaClient()
 export const selectel = new SelectelStorage(sel_user, sel_pass)
+
 container.bind("Prisma").toConstantValue(prisma)
 container.bind("Selectel").toConstantValue(selectel)
-container.bind("Model/Mods/Mod").to(ModelMod)
+container.bind("Email").toConstantValue(email_transport)
+container.load(buildProviderModule())
+
