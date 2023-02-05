236667481b4f4d5436567c2c3b301319a7bd810c
ron-tayler <max.urs@yandex.ru>
2023-02-05 17:26:54 +0300

    Edit cors middleware

--- src/App/Middlewares/Cors.ts
+++ src/App/Middlewares/Cors.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response} from 'express'
 
-export default function Cors(req: Request, res: Response, next: NextFunction) {
-    res.setHeader("Content-Type","application/json;charset=UTF-8")
+export default function CorsMiddleware(req: Request, res: Response, next: NextFunction) {
+    // res.setHeader("Content-Type","application/json;charset=UTF-8")
     res.setHeader("Access-Control-Allow-Origin","*")
     res.setHeader("Access-Control-Allow-Methods","GET,POST")
     res.setHeader("Access-Control-Allow-Headers","Content-Type")
