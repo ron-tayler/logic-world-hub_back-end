d184cd51f3ff91579f2a9acb2233e68b87352fc0
ron-tayler <max.urs@yandex.ru>
2023-02-05 17:28:17 +0300

    Use provider

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,33 @@
 import moduleAlias from 'module-alias';
-moduleAlias.addAlias('@', __dirname)
-moduleAlias()
+moduleAlias.addAlias('@', __dirname);
+moduleAlias();
 import "reflect-metadata";
 
 import bodyParser from 'body-parser';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import { container } from "@/container";
-import Cors from "@/App/Middlewares/corsMiddleware";
+import CorsMiddleware from "@/App/Middlewares/Cors";
+import ErrorMiddleware from "@/App/Middlewares/Error";
+import NotFoundMiddleware from "@/App/Middlewares/NotFound"
+import {AuthProvider} from "@/App/AuthProvider";
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 let server = new InversifyExpressServer(container,null,{
     rootPath: "/api/v1"
-});
-server.setConfig((app) => {
+}, null, AuthProvider);
+
+server.setConfig(app => {
+    app.use(CorsMiddleware);
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
-    app.use(Cors)
 });
 
-let app = server.build();
-app.listen(SERVER_PORT,() => {
-    console.log(`Server started on port ${SERVER_PORT}`)
+server.setErrorConfig(app => {
+    app.use(NotFoundMiddleware);
+    app.use(ErrorMiddleware);
+});
+
+server.build().listen(SERVER_PORT,() => {
+    console.log(`Server start listened on port ${SERVER_PORT}`);
 });
